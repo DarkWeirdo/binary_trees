87acc0de5ba83db464fe4f3c6703eb67c00ec566
DarkWeirdo <sonic_unleashed@live.com>
2024-03-03 23:09:45 +0200

    task 9 refix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -19,7 +19,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	height_r = binary_tree_height(tree->right);
 
 	if (height_l > height_r)
-		return (height_l);
+		return (height_l + 1);
 	else
-		return (height_r);
+		return (height_r + 1);
 }
