e930d5755c3d549cb0e517ca64bb6c9a687df34c
DarkWeirdo <sonic_unleashed@live.com>
2024-03-04 00:31:22 +0200

    style fix

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,10 +8,11 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    else if (tree->parent == NULL)
-        return (0);
-    else
-        return (1 + binary_tree_depth(tree->parent));
+	if (tree == NULL)
+		return (0);
+	else if (tree->parent == NULL)
+		return (0);
+	else
+		return (1 + binary_tree_depth(tree->parent));
 }
+
