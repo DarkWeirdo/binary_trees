e0eb36d548c4671b3a03134c0d11c0323cf1f06b
DarkWeirdo <sonic_unleashed@live.com>
2024-03-03 23:53:55 +0200

    task 9 fix no unknown

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,24 +2,19 @@
 
 /**
  * binary_tree_height - Measures the height of a binary tree
- *
  * @tree: Pointer to the root node of the tree to measure the height
  *
- * Return: The height of the tree starting at @tree
+ * Return: The height of the tree
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l, height_r;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
-		return (0);
+		return 0;
 
-	height_l = binary_tree_height(tree->left);
-	height_r = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	if (height_l > height_r)
-		return (height_l + 1);
-	else
-		return (height_r + 1);
+	return (left_height > right_height ? left_height : right_height) + 1;
 }
