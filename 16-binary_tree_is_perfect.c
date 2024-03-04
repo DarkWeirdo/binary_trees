9744d082dd5617d7d7f2dee649592e4c2392276e
DarkWeirdo <sonic_unleashed@live.com>
2024-03-04 04:35:28 +0200

    task 16 fix 1

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,14 +8,14 @@
  */
 static size_t _height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+    size_t left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return (0);
 
-	left_height = tree->left ? 1 + _height(tree->left) : 1;
-	right_height = tree->right ? 1 + _height(tree->right) : 1;
-	return ((left_height > right_height) ? left_height : right_height);
+    left_height = tree->left ? 1 + _height(tree->left) : 0;
+    right_height = tree->right ? 1 + _height(tree->right) : 0;
+    return ((left_height > right_height) ? left_height : right_height);
 }
 
 /**
@@ -26,18 +26,17 @@ static size_t _height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height;
+    size_t height;
 
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return (0);
 
-	height = _height(tree);
-	if (height == 0)
-		return (1);
+    height = _height(tree);
+    if (height == 0)
+        return (1);
 
-	return (binary_tree_is_perfect(tree->left) &&
-			binary_tree_is_perfect(tree->right) &&
-			_height(tree->left) == _height(tree->right) &&
-			_height(tree->left) == height - 1);
+    return (binary_tree_is_perfect(tree->left) &&
+            binary_tree_is_perfect(tree->right) &&
+            _height(tree->left) == _height(tree->right) &&
+            _height(tree->left) == height - 1);
 }
-
